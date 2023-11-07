f2427cc60fbe79c5de12107123935a010b1b770c
prathmeshtiwari22 <122382508+prathmeshtiwari22@users.noreply.github.com>
2023-11-07 12:36:41 +0530

    Bank.C

--- Bank.C
+++ Bank.C
@@ -1,23 +1,48 @@
-Arduino Servo motor Rotary code for our Bank locker security system
-
-
-#include<SoftwareSerial.h>
 #include <Servo.h>
-Servo myservo;
-SoftwareSerial mySerial(2, 3); // RX, TX
-int Position ;
-void setup()
-{
-myservo.attach(11);
-mySerial.begin(9600);
-Serial.begin(9600);
+
+Servo lockerServo;
+int servoPosition = 90; // Initial servo position (locked position)
+bool isLocked = true;   // Initial lock state
+
+void setup() {
+  lockerServo.attach(9); // Connect the servo to pin 9
+  Serial.begin(9600);
 }
-void loop()
-{
-if(mySerial.available()>0)
-{
-Position = mySerial.read();
-Position = map(Position, 0, 180, 180, 0);
-myservo.write(Position);
+
+void loop() {
+  // Check for commands sent over Serial
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    executeCommand(command);
+  }
 }
+
+void executeCommand(char command) {
+  if (command == 'O') {
+    unlockLocker();
+  } else if (command == 'L') {
+    lockLocker();
+  } else {
+    Serial.println("Invalid command");
+  }
+}
+
+void unlockLocker() {
+  if (isLocked) {
+    lockerServo.write(0); // Rotate the servo to unlock position
+    isLocked = false;
+    Serial.println("Locker unlocked");
+  } else {
+    Serial.println("Locker is already unlocked");
+  }
+}
+
+void lockLocker() {
+  if (!isLocked) {
+    lockerServo.write(90); // Rotate the servo to lock position
+    isLocked = true;
+    Serial.println("Locker locked");
+  } else {
+    Serial.println("Locker is already locked");
+  }
 }
